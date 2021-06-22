2ae94216379ae6b3f47c4d8d3becacc0e14fc1b3
Ramon Marcu <ramondotmarcu@gmail.com>
2021-06-22 17:10:11 +0300

    Add files via upload

---
+++ blocks and scope.js
@@ -0,0 +1,12 @@
+//main js.
+
+const city = 'New York City';
+
+const logCitySkyline = () => {
+  let skyscraper = 'Empire State Building';
+   return 'The stars over the ' + skyscraper + ' in ' + city;
+};
+
+console.log(logCitySkyline());
+
+// The stars over the Empire State Building in New York City
